fe2e8760786feb47a39a7e59f4d61142e8cad62f
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-24 11:15:38 -0300

    despues de validar el ingreso de todos los datos al formulario se traspasan esos valores a la clase Animal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 import Animal from "./animal.js";
-import Sonidos from "./sonidos.js";
+//import Sonidos from "./sonidos.js";
 
 const animal = new Animal();
 const select = document.getElementById("animal");
 const edad = document.getElementById("edad");
+const comentario = document.getElementById("comentarios");
+const agregar = document.getElementById("btnRegistrar");
+
 let nameAnimal = "";
 let ageAnimal = "";
+let constrAlert = "";
+let validar = false;
 
 //se determina que animal escogio el usuario
 select.addEventListener("change", function () {
@@ -19,12 +24,31 @@ edad.addEventListener("change", function () {
   ageAnimal = selectedOption.value;
 });
 
-const agregar = document.getElementById("btnRegistrar");
+function validarFormulario(name, age, comment) {
+  if (name == "" || name == "Seleccione un animal" || name == null) {
+    constrAlert = `${constrAlert} \n Debe seleccionar un animal`;
+  }
+  if (age == "" || age == "Seleccione un rango de años" || age == null) {
+    constrAlert = `${constrAlert} \n Debe seleccionar una edad`;
+  }
+  if (comment == "" || comment == " " || comment == null) {
+    constrAlert = `${constrAlert} \n Debe agregar un comentario`;
+  }
+  if (constrAlert.length == 0) {
+    validar = true;
+  } else {
+    alert(constrAlert);
+  }
+}
 
 agregar.addEventListener("click", () => {
-  const name = nameAnimal;
-  const age = ageAnimal;
-  /*const image = document.getElementById("image").value;
-    const comment = document.getElementById("comment").value;
-    const sound = document.getElementById("sound").value; */
+    if (validar == false) {
+        validarFormulario(nameAnimal, ageAnimal, comentario.value);
+     }
+    else {  
+        animal.name = nameAnimal;
+        animal.age = ageAnimal;
+        animal.comment = comentario.value;
+    }
 });
+
