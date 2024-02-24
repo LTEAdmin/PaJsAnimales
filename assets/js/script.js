abb6c1ba1ce5e7894f2d6f7971bed6ccfa76c634
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-24 10:54:30 -0300

    se cambia for de label en comentarios para que este funcione correctamente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,30 @@
 import Animal from "./animal.js";
 import Sonidos from "./sonidos.js";
 
-
 const animal = new Animal();
 const select = document.getElementById("animal");
-const edad=document.getElementById("edad");
+const edad = document.getElementById("edad");
 let nameAnimal = "";
 let ageAnimal = "";
 
 //se determina que animal escogio el usuario
 select.addEventListener("change", function () {
   let selectedOption = this.options[select.selectedIndex];
-  nameAnimal=selectedOption.value;
+  nameAnimal = selectedOption.value;
 });
 
 //se determina el rango de edad que ingreso el usuario
 edad.addEventListener("change", function () {
-    let selectedOption = this.options[edad.selectedIndex];
-    ageAnimal=selectedOption.value;
-})
-
+  let selectedOption = this.options[edad.selectedIndex];
+  ageAnimal = selectedOption.value;
+});
 
 const agregar = document.getElementById("btnRegistrar");
 
 agregar.addEventListener("click", () => {
-    const name = document.getElementById("nameAnimal").value;
-    /* const age = document.getElementById("age").value;
-    const image = document.getElementById("image").value;
+  const name = nameAnimal;
+  const age = ageAnimal;
+  /*const image = document.getElementById("image").value;
     const comment = document.getElementById("comment").value;
     const sound = document.getElementById("sound").value; */
 });
