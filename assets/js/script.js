5f1d6c3a3b51fbb62991ef80ed72c6d1e391417b
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-24 10:51:57 -0300

    se crean eventos para determinar datos seleccionados por el usuario de tipo de animal y de edad

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,24 @@ import Sonidos from "./sonidos.js";
 
 
 const animal = new Animal();
+const select = document.getElementById("animal");
+const edad=document.getElementById("edad");
+let nameAnimal = "";
+let ageAnimal = "";
+
+//se determina que animal escogio el usuario
+select.addEventListener("change", function () {
+  let selectedOption = this.options[select.selectedIndex];
+  nameAnimal=selectedOption.value;
+});
+
+//se determina el rango de edad que ingreso el usuario
+edad.addEventListener("change", function () {
+    let selectedOption = this.options[edad.selectedIndex];
+    ageAnimal=selectedOption.value;
+})
+
+
 const agregar = document.getElementById("btnRegistrar");
 
 agregar.addEventListener("click", () => {
